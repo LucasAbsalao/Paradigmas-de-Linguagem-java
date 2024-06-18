07b8009dda7302749ff8b16e348898762e2f82a1
LightAsh04 <lsa4@cin.ufpe.br>
2024-06-17 21:28:52 -0300

    aula deletada

--- Aula1/Main.java
+++ Aula1/Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String args[]){
-        Conta ana = new Conta("71347467475", 24.24);
+        Conta ana = new Conta("123", 24.24);
         Conta teste2 = new Conta();
         ana.creditar(30);
         System.out.println(teste2.getSaldo());
