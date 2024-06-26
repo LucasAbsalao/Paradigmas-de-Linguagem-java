91565c444b4425521b5b93cabe4d7a9accd88709
lsa4 <lsa4@g3c24.cin.ufpe.br>
2024-06-26 11:54:28 -0300

    aula 5

---
+++ Aula5/ContaAbstrata.java
@@ -0,0 +1,27 @@
+public abstract class ContaAbstrata {
+    private String numero;
+    private double saldo;
+
+    public ContaAbstrata (String num){
+        this.numero = num;
+        this.saldo = 0.0;
+    }
+
+    public void creditar (double valor){
+        this.saldo += valor;
+    }
+
+    public double getSaldo(){
+        return this.saldo;
+    }
+
+    public String getNumero(){
+        return this.numero;
+    }
+
+    protected void setSaldo(double valor){
+        this.saldo = valor;
+    }
+
+    public abstract void debitar(double valor);
+}
